4de4e722b365a8f0266ac4cfb897f0e4c853c299
Immerfallen <marodemelo10@gmail.com>
2021-02-17 10:30:56 -0300

    fibonacci

--- programacaoRecursiva/fibonacci.java
+++ programacaoRecursiva/fibonacci.java
@@ -5,4 +5,26 @@ processos para comparar o consumo de memória e o processamento.  */
 
 public class fibonacci {
     
+    public static int iterativo(int n){
+        int f = -1;
+        int fibMenos1 =1;
+        int fibMenos2 = 0;
+        for(int i = 3; i<=n;i++){
+            f = fibMenos1 + fibMenos2;
+            fibMenos2 = fibMenos1;
+            fibMenos1 = f;
+        } 
+        return f;
+    }
+
+    public static int recursivoBinario(int n){
+        if(n==0 || n==1) return 0;
+        if (n==2) return 1;
+        return recursivoBinario(n-2) + recursivoBinario(n-1);
+    }
+
+    public static void main(String[] args){
+        System.out.println(recursivoBinario(13));
+    }
+
 }
